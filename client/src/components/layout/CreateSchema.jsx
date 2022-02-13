0822b6dde5fdd663c445a9dbc6b85e8719a67b7a
Crehan <crehanas14@gmail.com>
2022-02-14 00:09:58 +0530

    add did check before create schema

--- client/src/components/layout/CreateSchema.jsx
+++ client/src/components/layout/CreateSchema.jsx
@@ -1,7 +1,7 @@
 import React , {useEffect, useState} from "react";
 import { Link } from "react-router-dom";
 import classnames from "classnames";
-import { Row , Col , FormControl , InputGroup , Button , Form , Card , Container } from "react-bootstrap";
+import { Alert , Row , Col , FormControl , InputGroup , Button , Form , Card , Container } from "react-bootstrap";
 import axios from "axios";
 import { useSelector } from "react-redux";
 
@@ -85,107 +85,114 @@ const CreateSchema = () => {
 
     return (<>
         <Container className="d-flex flex-column align-items-center">
-        <Card className="shadow w-75 mt-5 mb-5">
-            <Card.Header className='pl-5 pt-3 pb-2 d-flex flex-column align-items-center bg-dark text-white'>
-                <h2><strong>Create Schema</strong></h2>
-            </Card.Header>
-            <Card.Body className='px-5'>
-                <Form onSubmit={onSubmit}>
+        { (user.did!=="")?(
+            <Card className="shadow w-75 mt-5 mb-5">
+                <Card.Header className='pl-5 pt-3 pb-2 d-flex flex-column align-items-center bg-dark text-white'>
+                    <h2><strong>Create Schema</strong></h2>
+                </Card.Header>
+                <Card.Body className='px-5'>
+                    <Form onSubmit={onSubmit}>
 
-                    <Form.Group as={Row} className="mb-3">
-                        <Form.Label column sm="2">
-                            Name
-                        </Form.Label>
-                        <Col sm="10">
-                            <Form.Control
-                                type="text"
-                                value = {name}
-                                onChange = { e => setName(e.target.value) }
-                            />
-                        </Col>
-                    </Form.Group>
-                    <Form.Group as={Row} className="mb-4">
-                        <Form.Label column sm="2">
-                            Description
-                        </Form.Label>
-                        <Col sm="10">
-                            <FormControl
-                                as="textarea"
-                                value = {description}
-                                onChange = { e => setDescription(e.target.value) }
-                            />
-                        </Col>
-                    </Form.Group>
-                    {
-                        form.map( (value,index) => {
-                            return (
-                            <>
-                                <hr />
-                                <Form.Group key={index} as={Row} className="mb-3">
-                                    <Form.Label column sm="2">
-                                        Key
-                                    </Form.Label>
-                                    <Col sm="10">
-                                        <Form.Control
-                                            type="text"
-                                            value={value.key}
-                                            name="key"
-                                            onChange={(e) => onPropertyChange(e,index)}
-                                        />
-                                    </Col>
-                                </Form.Group>
-                                <Form.Group key={index} as={Row} className="mb-3">
-                                    <Form.Label column sm="2">
-                                        Type
-                                    </Form.Label>
-                                    <Col sm="10">
-                                        <Form.Control
-                                            type="text"
-                                            value={value.propType}
-                                            name="propType"
-                                            onChange={(e) => onPropertyChange(e,index)}
-                                        />
-                                    </Col>
-                                </Form.Group>
-                                <Form.Group key={index} as={Row} className="mb-3">
-                                    <Form.Label column sm="2">
-                                        Format
-                                    </Form.Label>
-                                    <Col sm="10">
-                                        <Form.Control
-                                            type="text"
-                                            value={value.propFormat}
-                                            name="propFormat"
-                                            onChange={(e) => onPropertyChange(e,index)}
-                                        />
-                                    </Col>
-                                </Form.Group>
-                                <div className='d-flex flex-column align-items-end mt-3'>
-                                    <Button
-                                        variant="danger"
-                                        type="submit"
-                                        onClick={ (e) => removeProperty(e,index) }
-                                    >
-                                        Remove
-                                    </Button>
-                                </div>
-                            </>);
-                        })
-                    }
-                    <hr />
-                    <div className='d-flex flex-column align-items-end mt-3'>
-                        <Button variant="success" type="submit" onClick={addProperty}>
-                            Add Property
-                        </Button>
-                    </div>
-                    <div className='d-flex flex-column align-items-center mt-3'>
-                        <Button variant="primary" type="submit">
-                            Submit
-                        </Button>
-                    </div>
-                </Form>
-            </Card.Body>
-        </Card>
+                        <Form.Group as={Row} className="mb-3">
+                            <Form.Label column sm="2">
+                                Name
+                            </Form.Label>
+                            <Col sm="10">
+                                <Form.Control
+                                    type="text"
+                                    value = {name}
+                                    onChange = { e => setName(e.target.value) }
+                                />
+                            </Col>
+                        </Form.Group>
+                        <Form.Group as={Row} className="mb-4">
+                            <Form.Label column sm="2">
+                                Description
+                            </Form.Label>
+                            <Col sm="10">
+                                <FormControl
+                                    as="textarea"
+                                    value = {description}
+                                    onChange = { e => setDescription(e.target.value) }
+                                />
+                            </Col>
+                        </Form.Group>
+                        {
+                            form.map( (value,index) => {
+                                return (
+                                <>
+                                    <hr />
+                                    <Form.Group key={index} as={Row} className="mb-3">
+                                        <Form.Label column sm="2">
+                                            Key
+                                        </Form.Label>
+                                        <Col sm="10">
+                                            <Form.Control
+                                                type="text"
+                                                value={value.key}
+                                                name="key"
+                                                onChange={(e) => onPropertyChange(e,index)}
+                                            />
+                                        </Col>
+                                    </Form.Group>
+                                    <Form.Group key={index} as={Row} className="mb-3">
+                                        <Form.Label column sm="2">
+                                            Type
+                                        </Form.Label>
+                                        <Col sm="10">
+                                            <Form.Control
+                                                type="text"
+                                                value={value.propType}
+                                                name="propType"
+                                                onChange={(e) => onPropertyChange(e,index)}
+                                            />
+                                        </Col>
+                                    </Form.Group>
+                                    <Form.Group key={index} as={Row} className="mb-3">
+                                        <Form.Label column sm="2">
+                                            Format
+                                        </Form.Label>
+                                        <Col sm="10">
+                                            <Form.Control
+                                                type="text"
+                                                value={value.propFormat}
+                                                name="propFormat"
+                                                onChange={(e) => onPropertyChange(e,index)}
+                                            />
+                                        </Col>
+                                    </Form.Group>
+                                    <div className='d-flex flex-column align-items-end mt-3'>
+                                        <Button
+                                            variant="danger"
+                                            type="submit"
+                                            onClick={ (e) => removeProperty(e,index) }
+                                        >
+                                            Remove
+                                        </Button>
+                                    </div>
+                                </>);
+                            })
+                        }
+                        <hr />
+                        <div className='d-flex flex-column align-items-end mt-3'>
+                            <Button variant="success" type="submit" onClick={addProperty}>
+                                Add Property
+                            </Button>
+                        </div>
+                        <div className='d-flex flex-column align-items-center mt-3'>
+                            <Button variant="primary" type="submit">
+                                Submit
+                            </Button>
+                        </div>
+                    </Form>
+                </Card.Body>
+            </Card>
+            ):(
+                <Alert variant="danger" className="mt-3">
+                    You need to Create a DID before you can create a schema
+                </Alert>
+            )
+        }
         </Container>
     </>);
 }
